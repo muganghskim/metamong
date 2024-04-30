b70d4c5f4caff4fd7949e19c9e7f789705f1f786
STD-A <rhgustmfrh@naver.com>
2024-04-30 10:48:15 +0900

    first commit

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import ThreeScene from "../../components/ThreeScene";
+
+const Home: React.FC = () => {
+    return (
+      <>
+        <ThreeScene></ThreeScene>
+      </>
+    );
+  };
+  
+  export default Home;
